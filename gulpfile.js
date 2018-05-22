5ff766394451169461acaf4d6ce44d78a25f72ab
Asjas <asjas@outlook.com>
2018-05-22 22:38:18 +0200

    remove redundant gulp-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -3,61 +3,65 @@ const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 const gulp = require('gulp');
-const livereload = require('gulp-livereload');
 const plumber = require('gulp-plumber');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
-// HTML
-gulp.task('reload-html', () => {
-  gulp.src('index.html').pipe(livereload());
-});
-
 // Styles
 gulp.task('styles', () =>
   gulp
     .src('./css/app.css')
-    .pipe(plumber(function (err) {
-      console.log('Styles Task Error');
-      console.log(err);
-      this.emit('end');
-    }))
+    .pipe(
+      plumber(function(err) {
+        console.log('Styles Task Error');
+        console.log(err);
+        this.emit('end');
+      }),
+    )
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
     .pipe(cleanCSS({ compatibility: 'ie11' }))
     .pipe(cleanCSS({ level: '2' }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('./', {
-      sourceMappingURL(file) {
-        return `${file.relative}.map`;
-      },
-    }))
-    .pipe(gulp.dest('./css/'))
-    .pipe(livereload()));
+    .pipe(
+      sourcemaps.write('./', {
+        sourceMappingURL(file) {
+          return `${file.relative}.map`;
+        },
+      }),
+    )
+    .pipe(gulp.dest('./css/')),
+);
 
 // JavaScript
 gulp.task('minify-js', () =>
   gulp
     .src('./js/app.js')
-    .pipe(plumber(function (err) {
-      console.log('JavaScript Task Error');
-      console.log(err);
-      this.emit('end');
-    }))
+    .pipe(
+      plumber(function(err) {
+        console.log('JavaScript Task Error');
+        console.log(err);
+        this.emit('end');
+      }),
+    )
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015'],
-    }))
+    .pipe(
+      babel({
+        presets: ['es2015'],
+      }),
+    )
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('./', {
-      sourceMappingURL(file) {
-        return `${file.relative}.map`;
-      },
-    }))
-    .pipe(gulp.dest('./js/'))
-    .pipe(livereload()));
+    .pipe(
+      sourcemaps.write('./', {
+        sourceMappingURL(file) {
+          return `${file.relative}.map`;
+        },
+      }),
+    )
+    .pipe(gulp.dest('./js/')),
+);
 
 // Build task
 gulp.task('build', ['styles', 'minify-js'], () => {
@@ -68,8 +72,5 @@ gulp.task('build', ['styles', 'minify-js'], () => {
 gulp.task('watch', ['build'], () => {
   console.log('Starting watch task');
   require('./server.js');
-  livereload.listen();
-  gulp.watch('index.html', ['reload-html']),
-  gulp.watch('css/app.css', ['styles']),
-  gulp.watch('js/app.js', ['minify-js']);
+  gulp.watch('css/app.css', ['styles']), gulp.watch('js/app.js', ['minify-js']);
 });
