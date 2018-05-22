ed72fa0b98b8bae8f1c895211183a3fa74a363a9
Asjas <asjas@outlook.com>
2018-05-22 22:34:19 +0200

    add eslint and eslint packages

---
+++ .eslintrc.js
@@ -0,0 +1,89 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "prettier",
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "impliedStrict": true,
+      "classes": true
+    }
+  },
+  "env": {
+    "browser": true,
+  },
+  "rules": {
+    "no-unused-vars": [
+      1,
+      {
+        "argsIgnorePattern": "res|next|^err"
+      }
+    ],
+    "arrow-body-style": [
+      2,
+      "as-needed"
+    ],
+    "no-param-reassign": [
+      2,
+    ],
+    "no-console": 0,
+    "import/prefer-default-export": 0,
+    "import": 0,
+    "no-plusplus": 0,
+    "no-param-reassign": 0,
+    "class-methods-use-this": ["error", {
+      "exceptMethods": ["render"]
+    }],
+    "func-names": 0,
+    "space-before-function-paren": 0,
+    "no-use-before-define": 0,
+    "comma-dangle": ["error", {
+        "arrays": "never",
+        "objects": "always",
+        "imports": "never",
+        "exports": "never",
+        "functions": "ignore"
+    }],
+    "max-len": 0,
+    "import/extensions": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "radix": 0,
+    "no-shadow": [
+      2,
+      {
+        "hoist": "all",
+        "allow": [
+          "resolve",
+          "reject",
+          "done",
+          "next",
+          "err",
+          "error"
+        ]
+      }
+    ],
+    "quotes": [
+      2,
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        "trailingComma": "all",
+        "singleQuote": true,
+        "printWidth": 120,
+      }
+    ],
+  },
+  "plugins": [
+    "prettier"
+  ]
+}
